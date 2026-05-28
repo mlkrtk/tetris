d88b3f0a5c33eced17f5afbeb5158545d41c12cd
Karthik <alex.mlkrtk@live.com>
2026-05-28 20:08:10 +0530

    updated start button

--- src/components/StartPage/index.js
+++ src/components/StartPage/index.js
@@ -73,7 +73,7 @@ const StartButton = styled.button`
 const StartPage = ({ startClick }) => {
 	return (
 		<StyledStartPage>
-			<StartButton onClick={startClick}>PLAY</StartButton>
+			<StartButton onClick={startClick}>START-NOW</StartButton>
 		</StyledStartPage>
 	);
 };
